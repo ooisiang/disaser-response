5d2a733e3b517856a4158521c106eb0f2cbc6179
Ooi Siang Tan <ooisiangtan@gmail.com>
2020-05-25 22:37:04 +0200

    [ADD]Implement clean_data()

--- data/process_data.py
+++ data/process_data.py
@@ -30,7 +30,53 @@ def load_data(messages_filepath, categories_filepath):
 
 
 def clean_data(df):
-    pass
+    """
+    This function aims to clean the data by removing rows with NaN in 'message' and 'categories' column and
+    duplicate disaster messages.
+    This function splits the single column with 36 categories to 36 columns and the value of each category column
+    will be extracted.
+
+    Args:
+        df (df) -- a pandas dataframe that consists of disaster messages and corresponding categories,
+        returned from load_data()
+
+    Return:
+        df_cleaned (df) -- a clean pandas dataframe with the newly added 36 disaster category columns
+
+    """
+
+    # Drop rows that do not have valid data in messages or categories col
+    if (df['message'].isna().sum() != 0) | (df['categories'].isna().sum() != 0):
+        df[['message', 'categories']].dropna(inplace=True)
+        print("    NaN in 'message' and 'categories' column found. Dropping NaN rows...")
+
+    # Create a dataframe of the 36 individual category columns
+    categories = pd.Series(df['categories']).str.split(pat=';', expand=True)
+
+    # Use one row of the categories dataframe to extract a list of new column names for categories
+    row = categories.loc[1, :]
+    category_colnames = row.apply(lambda x: x[:-2])
+
+    # Rename the columns of categories
+    categories.columns = category_colnames
+
+    # Convert category values to numbers, just 0 or 1 which is the last character of the string
+    for column in categories:
+        categories[column] = categories[column].apply(lambda x: x[-1])
+        categories[column] = categories[column].astype(int)
+
+    # Drop the original 'categories' column from original dataframe, df
+    df.drop(columns=['categories'], axis=1, inplace=True)
+
+    # Concatenate the original dataframe, df with the new categories dataframe
+    df_cleaned = pd.concat([df, categories], axis=1)
+
+    # Remove duplicates (id) in the dataset
+    if df_cleaned.duplicated(subset='id').sum() != 0:
+        df_cleaned.drop_duplicates(subset='id', inplace=True)
+        print("    Duplicate disaster messages found in dataset. Deleting duplicates...")
+
+    return df_cleaned
 
 
 def save_data(df, database_filename):
